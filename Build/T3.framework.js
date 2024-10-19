6462bca1fe0e921ca5c285b8c6b57e8a83f3a0d6
DigitalSharkLtd <threedmakerg@gmail.com>
2024-10-19 05:40:44 +0300

    Removed old files from root directory

--- Build/T3.framework.js
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:5634c962c652352be4af2ea9df6601be02e17c39c9e89c5ff3412192a5e43326
-size 411882
--- Build/T3.loader.js
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:8c700dffe34e672049ef5cc78911f122cbac398cfcdd79860ab38e6fda533816
-size 19232
